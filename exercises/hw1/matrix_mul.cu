c3c3394b8c9d37f828b1fd4883e544dda2aee67e
daniel.huang <daniel.huang@gallopwave.com>
2026-07-23 16:23:16 +0800

    Update HW1

--- exercises/hw1/matrix_mul.cu
+++ exercises/hw1/matrix_mul.cu
@@ -24,20 +24,20 @@ const float B_val = 2.0f;
 
 // matrix multiply (naive) kernel: C = A * B
 __global__ void mmul(const float *A, const float *B, float *C, int ds) {
+  int idx = threadIdx.x + blockIdx.x * blockDim.x;  // global col index
+  int idy = threadIdx.y + blockIdx.y * blockDim.y;  // global row index
 
-  int idx = threadIdx.x+blockDim.x*blockIdx.x; // create thread x index
-  int idy = threadIdx.y+blockDim.y*blockIdx.y; // create thread y index
-
-  if ((idx < ds) && (idy < ds)){
+  if ((idx < ds) && (idy < ds)) {
     float temp = 0;
-    for (int i = 0; i < ds; i++)
-      temp += A[FIXME*ds+i] * B[i*ds+FIXME];   // dot product of row and column
-    C[idy*ds+idx] = temp;
+    for (int i = 0; i < ds; ++i) {
+      temp += A[idy * ds + i] * B[i * ds + idx];
+      C[idy * ds + idx] = temp;
+    }
   }
 }
 
-int main(){
-
+// matrix multiply (naive) kernel: C = A * B
+int main() {
   float *h_A, *h_B, *h_C, *d_A, *d_B, *d_C;
 
   // these are just for timing
@@ -48,53 +48,58 @@ int main(){
   // start timing
   t0 = clock();
 
-  h_A = new float[DSIZE*DSIZE];
-  h_B = new float[DSIZE*DSIZE];
-  h_C = new float[DSIZE*DSIZE];
-  for (int i = 0; i < DSIZE*DSIZE; i++){
+  h_A = new float[DSIZE * DSIZE];
+  h_B = new float[DSIZE * DSIZE];
+  h_C = new float[DSIZE * DSIZE];
+
+  for (int i = 0; i < DSIZE * DSIZE; ++i) {
     h_A[i] = A_val;
     h_B[i] = B_val;
-    h_C[i] = 0;}
+    h_C[i] = 0;
+  }
 
   // Initialization timing
   t1 = clock();
   t1sum = ((double)(t1-t0))/CLOCKS_PER_SEC;
   printf("Init took %f seconds.  Begin compute\n", t1sum);
 
-  // Allocate device memory and copy input data over to GPU
-  cudaMalloc(&d_A, DSIZE*DSIZE*sizeof(float));
-  cudaMalloc(&d_B, DSIZE*DSIZE*sizeof(float));
-  cudaMalloc(&d_C, DSIZE*DSIZE*sizeof(float));
+  // Allocate device memory
+  cudaMalloc(&d_A, DSIZE * DSIZE * sizeof(float));
+  cudaMalloc(&d_B, DSIZE * DSIZE * sizeof(float));
+  cudaMalloc(&d_C, DSIZE * DSIZE * sizeof(float));
+
   cudaCheckErrors("cudaMalloc failure");
-  cudaMemcpy(d_A, h_A, DSIZE*DSIZE*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_B, h_B, DSIZE*DSIZE*sizeof(float), cudaMemcpyHostToDevice);
+
+  // Copy input data from host to device;
+  cudaMemcpy(d_A, h_A, DSIZE * DSIZE * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, h_B, DSIZE * DSIZE * sizeof(float), cudaMemcpyHostToDevice);
+
   cudaCheckErrors("cudaMemcpy H2D failure");
 
-  // Cuda processing sequence step 1 is complete
-
-  // Launch kernel
-  dim3 block(block_size, block_size);  // dim3 variable holds 3 dimensions
-  dim3 grid((DSIZE+block.x-1)/block.x, (DSIZE+block.y-1)/block.y);
+  dim3 block(block_size, block_size);
+  dim3 grid((DSIZE + block.x - 1) / block.x, (DSIZE + block.y - 1) / block.y);
   mmul<<<grid, block>>>(d_A, d_B, d_C, DSIZE);
+
   cudaCheckErrors("kernel launch failure");
 
-  // Cuda processing sequence step 2 is complete
+  // Copy results from device to host
+  cudaMemcpy(h_C, d_C, DSIZE * DSIZE * sizeof(float), cudaMemcpyDeviceToHost);
 
-  // Copy results back to host
-  cudaMemcpy(h_C, d_C, DSIZE*DSIZE*sizeof(float), cudaMemcpyDeviceToHost);
+    // GPU timing
+    t2 = clock();
+    t2sum = ((double)(t2-t1))/CLOCKS_PER_SEC;
+    printf ("Done. Compute took %f seconds\n", t2sum);
 
-  // GPU timing
-  t2 = clock();
-  t2sum = ((double)(t2-t1))/CLOCKS_PER_SEC;
-  printf ("Done. Compute took %f seconds\n", t2sum);
+    cudaCheckErrors("kernel execution failure or cudaMemcpy H2D failure");
+    for (int i = 0; i < DSIZE*DSIZE; i++) {
+      if (h_C[i] != A_val*B_val*DSIZE) {
+        printf("mismatch at index %d, was: %f, should be: %f\n", i, h_C[i], A_val*B_val*DSIZE);
+        return -1;
+      }
+    }
 
-  // Cuda processing sequence step 3 is complete
-
-  // Verify results
-  cudaCheckErrors("kernel execution failure or cudaMemcpy H2D failure");
-  for (int i = 0; i < DSIZE*DSIZE; i++) if (h_C[i] != A_val*B_val*DSIZE) {printf("mismatch at index %d, was: %f, should be: %f\n", i, h_C[i], A_val*B_val*DSIZE); return -1;}
-  printf("Success!\n"); 
-
-  return 0;
+    printf("Success!\n"); 
+  
+    return 0;
 }
   
